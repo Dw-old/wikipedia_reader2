5315d8040ebb6f5ea72dde6fa92fdd9dc0ca0a29
nchsvj <kalimaga12@gmail.com>
2026-05-05 10:08:36 +0500

    5

--- lib/main.dart
+++ lib/main.dart
@@ -45,6 +45,53 @@ class ArticleViewModel extends ChangeNotifier {
   }
 }
 
+class ArticleView extends StatefulWidget {
+  ArticleView({super.key});
+
+  State<ArticleView> createState() => _ArticleViewState();
+}
+
+class _ArticleViewState extends State<ArticleView> {
+  final viewModel = ArticleViewModel(ArticleModel());
+
+  @override
+  void initState() {
+    super.initState();
+    viewModel.fetchArticle();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
+
+class ArticleWidget extends StatelessWidget {
+  final Summary summary;
+  const ArticleWidget({super.key, required this.summary});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Padding(
+        padding: EdgeInsets.all(8.0),
+        child: Column(
+          spacing: 10,
+          children: [
+            if (summary.hasImage == true) Image.network(summary.originalImage!.source),
+            Text(summary.titles.normalized, overflow: TextOverflow.ellipsis, style: Theme.of(context).textTheme.displaySmall),
+
+            if (summary.description != null)
+            Text(summary.description!, overflow: TextOverflow.ellipsis, style: Theme.of(context).textTheme.bodyMedium),
+
+            Text(summary.extract)
+          ],
+        ),
+      ),
+    );
+  }
+}
+
 class MainApp extends StatelessWidget {
   const MainApp({super.key});
 
