c039a32a02c4f930e6e815fbe10e5327403ce44e
nchsvj <kalimaga12@gmail.com>
2026-05-05 10:13:38 +0500

    6

--- lib/main.dart
+++ lib/main.dart
@@ -92,6 +92,28 @@ class ArticleWidget extends StatelessWidget {
   }
 }
 
+class ArticlePage extends StatelessWidget {
+  final Summary summary;
+  final VoidCallback nextArticle;
+
+  const ArticlePage({super.key, required this.summary, required this.nextArticle});
+
+  @override
+  Widget build(BuildContext context) {
+    return SingleChildScrollView (
+      child: Column(
+        children: [
+          ArticleWidget(summary: summary),
+          ElevatedButton(
+            onPressed: nextArticle, 
+            child: Text('Next random article')
+          )
+        ],
+      ),
+    );
+  }
+}
+
 class MainApp extends StatelessWidget {
   const MainApp({super.key});
 
